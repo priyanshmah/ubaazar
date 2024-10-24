c5e7d586586f1c1a510d3f9afadaf476c62fef05
priyanshmah <priyanshmah17@gmail.com>
2024-10-24 13:02:51 +0530

    api changes

--- src/app/api/checkout/pay/validate/route.js
+++ src/app/api/checkout/pay/validate/route.js
@@ -8,40 +8,53 @@ export async function GET(request) {
 
         const { searchParams } = new URL(request.url);
         const transactionId = searchParams.get('transactionId');
-        
+
         if (transactionId) {
-            
+
             let statusUrl = `https://api.phonepe.com/apis/hermes/pg/v1/status/${process.env.NEXT_PUBLIC_PHONEPE_MERCHANT_ID}/${transactionId}`;
 
-            let string = `/pg/v1/status/${process.env.NEXT_PUBLIC_PHONEPE_MERCHANT_ID}/${transactionId}` + process.env.NEXT_PUBLIC_PHONEPE_API_KEY ;
+            let string = `/pg/v1/status/${process.env.NEXT_PUBLIC_PHONEPE_MERCHANT_ID}/${transactionId}` + process.env.NEXT_PUBLIC_PHONEPE_API_KEY;
             const shaString = sha256(string);
             const xVerify = shaString + '###' + process.env.NEXT_PUBLIC_PHONEPE_API_INDEX;
 
-            const options = {
+            // const axios = require('axios');
+            let data = '';
+
+            let config = {
                 method: 'get',
-                url: statusUrl,
+                maxBodyLength: Infinity,
+                url: 'https://api.phonepe.com/apis/hermes/pg/v1/status/M226WBXVLE3XO/7um2mxlmjd',
                 headers: {
-                    accept: 'text/plain',
                     'Content-Type': 'application/json',
-                    'X-VERIFY': xVerify,
-                    'X-MERCHANT-ID': process.env.NEXT_PUBLIC_PHONEPE_MERCHANT_ID,
-                }
-            }
+                    'X-VERIFY': 'fb1e8226a8dbcf61cad36e9b185842cd19ec3556420b9e3564313769af4d6be8###1',
+                    'X-MERCHANT-ID': 'M226WBXVLE3XO'
+                },
+                data: data
+            };
 
-            const response = await axios.request(options)
+            axios.request(config)
+                .then((response) => {
+                    console.log(JSON.stringify(response.data));
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
 
-            if (response.data && response.data.code === "PAYMENT_SUCCESS") {
 
-            }
-            else if (response.data && response.data.code === "PAYMENT_ERROR") {
-                
-            }
-            
-            return NextResponse.json({ response: response.data})
+            // const response = await axios.request(options)
+
+            // if (response.data && response.data.code === "PAYMENT_SUCCESS") {
+
+            // }
+            // else if (response.data && response.data.code === "PAYMENT_ERROR") {
+
+            // }
+
+            // return NextResponse.json({ response: response.data })
 
         }
 
-        
+
 
     } catch (error) {
         console.error(error);
