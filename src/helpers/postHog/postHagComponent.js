4b6324e3dcc4d50624fa0414a3f586c84eaa1756
priyanshmah <priyanshmah17@gmail.com>
2024-12-22 23:12:43 +0530

    commit 2.0

--- src/helpers/postHog/postHagComponent.js
+++ src/helpers/postHog/postHagComponent.js
@@ -1,13 +1,12 @@
 'use client'
 
-import { usePathname, useSearchParams } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider, usePostHog } from 'posthog-js/react'
 import { useEffect } from 'react'
 
 export function PostHogProvider({ children }) {
   const pathName = usePathname();
-  const userSearchParams = useSearchParams();
   const postHog = usePostHog();
 
   useEffect(() => {
@@ -15,22 +14,22 @@ export function PostHogProvider({ children }) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       person_profiles: 'identified_only',
-      capture_pageview: false,
+      capture_pageview: true,
       capture_pageleave: true,
     });
 
     // capture the pageview
-    if (pathName && postHog) {
-      let url = window.origin + pathName;
-      if(userSearchParams.toString()){
-        url = url + `${userSearchParams.toString()}`
-      }
-      postHog.capture('$pageview', {
-        current_url: url
-      })
-    }
+    // if (pathName && postHog) {
+    //   let url = window.origin + pathName;
+    //   if(userSearchParams.toString()){
+    //     url = url + `${userSearchParams.toString()}`
+    //   }
+    //   postHog.capture('$pageview', {
+    //     current_url: url
+    //   })
+    // }
 
-  }, [pathName, userSearchParams, postHog])
+  }, [])
 
   return (
     <PHProvider client={posthog}>
