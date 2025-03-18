331590756702116c57493ade8adc650f914d7e7c
priyanshmah <priyanshmah17@gmail.com>
2025-03-19 05:07:59 +0530

    app0.0.1

--- src/app/(FrontEnd)/login/admin/orders/page.jsx
+++ src/app/(FrontEnd)/login/admin/orders/page.jsx
@@ -4,17 +4,17 @@ import axios from "axios";
 
 export default async function OrdersPage() {
   
-  const response = await axios.post(
-    "https://www.ubaazar.com/api/auth/admin/orders",
-    JSON.stringify({
-      page: 1,
-      limit: 10,
-    })
-  );
+  // const response = await axios.post(
+  //   "https://www.ubaazar.com/api/auth/admin/orders",
+  //   JSON.stringify({
+  //     page: 1,
+  //     limit: 10,
+  //   })
+  // );
  
   return (
     <div className="overflow-x-hidden mb-20">
-      <OrderScreen orders={response.data?.orders}/>
+      {/* <OrderScreen orders={response.data?.orders}/> */}
     </div>
   );
 }
